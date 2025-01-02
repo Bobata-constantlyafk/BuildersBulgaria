97680cf44871d0493fa3c70d82be84a7b00a86db
Boyan <b.dechev@student.fontys.nl>
2025-01-02 14:21:45 +0200

    build fix 7

--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="galleryMain">
     <NuxtLink :to="'/gallery/House-trad'">
-      <img class="gallery-image" src="/./House-trad.png" alt="house">
+      <img class="gallery-image" src="https://i.imgur.com/hwjiXcD.jpeg" alt="house">
     </NuxtLink>
     <NuxtLink :to="'/gallery/House-modern'">
-      <img class="gallery-image" src="/./House-modern.png" alt="house">
+      <img class="gallery-image" src="https://i.imgur.com/Bbm0k1i.png" alt="house">
     </NuxtLink>
     <NuxtLink :to="'/gallery/Italian-house'">
-      <img class="gallery-image" src="/./Italian-house.png" alt="house">
+      <img class="gallery-image" src="https://i.imgur.com/jAub3iO.png" alt="house">
     </NuxtLink>
     <NuxtLink :to="'/gallery/house-example'">
-      <img class="gallery-image" src="/./house-example.jpg" alt="house">
+      <img class="gallery-image" src="https://i.imgur.com/UaU2kIe.jpeg" alt="house">
     </NuxtLink>
   </div>
 </template>
