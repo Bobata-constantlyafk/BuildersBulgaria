f958898767bb685f98bf8f97d88f586b2aceebb2
Boyan <b.dechev@student.fontys.nl>
2025-01-02 13:58:30 +0200

    build fix 4

--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="galleryMain">
     <NuxtLink :to="'/gallery/House-trad'">
-      <img class="gallery-image" :src="require('@/public/House-trad.png')" alt="house">
+      <img class="gallery-image" :src="houseTradImage" alt="house">
     </NuxtLink>
     <NuxtLink :to="'/gallery/House-modern'">
-      <img class="gallery-image" :src="require('@/public/House-modern.png')" alt="house">
+      <img class="gallery-image" :src="houseModernImage" alt="house">
     </NuxtLink>
     <NuxtLink :to="'/gallery/Italian-house'">
-      <img class="gallery-image" :src="require('@/public/Italian-house.png')" alt="house">
+      <img class="gallery-image" :src="italianHouseImage" alt="house">
     </NuxtLink>
     <NuxtLink :to="'/gallery/house-example'">
-      <img class="gallery-image" :src="require('@/public/house-example.jpg')" alt="house">
+      <img class="gallery-image" :src="houseExampleImage" alt="house">
     </NuxtLink>
   </div>
 </template>
+
+<script>
+import houseTradImage from '@/public/House-trad.png';
+import houseModernImage from '@/public/House-modern.png';
+import italianHouseImage from '@/public/Italian-house.png';
+import houseExampleImage from '@/public/house-example.jpg';
+
+export default {
+  data() {
+    return {
+      houseTradImage,
+      houseModernImage,
+      italianHouseImage,
+      houseExampleImage,
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 .galleryMain {
@@ -26,8 +44,8 @@
 }
 
 .gallery-image {
-  width: 100%; 
+  width: 100%;
   height: 100%;
-  object-fit:cover;
+  object-fit: cover;
 }
 </style>
